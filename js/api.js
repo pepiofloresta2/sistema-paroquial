75933ba3a7b5ef22d43e311bacb2eec59c63e8b1
pepiofloresta2 <pepio.floresta2@gmail.com>
2026-04-24 12:12:29 -0300

    Add files via upload

--- js/api.js
+++ js/api.js
@@ -4,15 +4,19 @@ const API = {
 
         let url = CONFIG.API_URL + "?" + new URLSearchParams(params);
 
-        return fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                return data;
-            })
-            .catch(error => {
-                console.error("Erro de comunicação:", error);
-                alert("Erro ao comunicar com Google Sheets.");
-            });
+        return fetch(url, {
+            method: "GET",
+            mode: "no-cors"
+        })
+        .then(() => {
+            return {
+                status: "ok"
+            };
+        })
+        .catch(err => {
+            console.error("Erro de comunicação:", err);
+            alert("Erro ao conectar com Google Sheets.");
+        });
     }
 
 };
