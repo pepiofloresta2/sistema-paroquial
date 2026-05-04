d68af3d1c5e5266d8a911648823df547a8afb46d
pepiofloresta2 <pepio.floresta2@gmail.com>
2026-05-04 19:09:52 -0300

    Add files via upload

--- js/relatorio.js
+++ js/relatorio.js
@@ -146,19 +146,11 @@ const Relatorio = {
                                 <th style="border:1px solid #000; padding:6px;">ENTRADAS</th>
                                 <th style="border:1px solid #000; padding:6px;">SAÍDAS</th>
                             </tr>
-                        </thead>
+                        </thead>                        
 
-                        <div id="area-impressao">
-                            <table style="width:100%; border-collapse:collapse; margin-top:10px; font-size:12px;">
-
-                                <thead>...</thead>
-
-                                <tbody>
-                                    ${linhas}
-                                </tbody>
-
-                            </table>
-                        </div>                    
+                        <tbody>
+                            ${linhas}
+                        </tbody>                                               
 
                     </table>
                 </div>
