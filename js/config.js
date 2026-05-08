06dc357335eba16f80e35b24bbd49a0615d8771b
pepiofloresta2 <pepio.floresta2@gmail.com>
2026-05-07 22:58:54 -0300

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    API_URL: "https://script.google.com/macros/s/AKfycbzxeqlInVn7dItSh_JpTmOniTtcd5VGdEv9BnMHzuKHMuC4Upbgy_IJk_zYhy8YJvOM7w/exec"
+    API_URL: "https://script.google.com/macros/s/AKfycbxa2SZvdmkIOCyOMvXrydfR1SiOhPqqQSvAQ90jIBn-Kx4v2z3QEolFDDKfUdYtPl_-1Q/exec"
 };
