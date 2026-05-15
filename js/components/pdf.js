c134c443aded95ab16477bb7c54c3a7aa1bd2e4b
pepiofloresta2 <pepio.floresta2@gmail.com>
2026-05-15 20:20:20 -0300

    Add files via upload

--- js/components/pdf.js
+++ js/components/pdf.js
@@ -1,39 +1,50 @@
-PDF.imprimir = function () {
+const PDF = {
 
-    let element =
-        document.getElementById("doc");
+    imprimir() {
 
-    if (!element) {
-        alert("Gere um relatório primeiro");
-        return;
+        let element =
+            document.getElementById("doc");
+
+        if (!element) {
+            alert(
+                "Gere um relatório primeiro"
+            );
+            return;
+        }
+
+        html2pdf()
+            .set({
+                margin: [5, 5, 5, 5],
+
+                filename:
+                    "relatorio.pdf",
+
+                image: {
+                    type: "jpeg",
+                    quality: 0.98
+                },
+
+                html2canvas: {
+                    scale: 2,
+                    useCORS: true
+                },
+
+                jsPDF: {
+                    unit: "mm",
+                    format: "a4",
+                    orientation:
+                        "portrait"
+                },
+
+                pagebreak: {
+                    mode: [
+                        "css",
+                        "legacy"
+                    ]
+                }
+            })
+            .from(element)
+            .save();
     }
 
-    html2pdf()
-        .set({
-            margin: [5, 5, 5, 5],
-
-            filename: "relatorio.pdf",
-
-            image: {
-                type: "jpeg",
-                quality: 0.98
-            },
-
-            html2canvas: {
-                scale: 2,
-                useCORS: true
-            },
-
-            jsPDF: {
-                unit: "mm",
-                format: "a4",
-                orientation: "portrait"
-            },
-
-            pagebreak: {
-                mode: ["css", "legacy"]
-            }
-        })
-        .from(element)
-        .save();
 };
